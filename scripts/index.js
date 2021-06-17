81b8c3e49e59eba1e4c44246183bec0fa84e9bef
Nikolay Sibekin <nik-sibekin@yandex.ru>
2021-06-17 03:53:19 +0300

    feat: first commit

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,21 @@ popupEditButtonElement.addEventListener('click', togglePopupVisibility);
 popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
 popupSubmitButtonElement.addEventListener('click', togglePopupVisibility);
 
+let formElement = document.querySelector('form');
+let nameInput = formElement.elements.name;
+let jobInput = formElement.elements.job;
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    let userName = nameInput.value;
+    let userJob = jobInput.value;
+
+    nameInput = formElement.elements.name;
+    jobInput = formElement.elements.job;
+    
+    nameInput.textContent = userName;
+    jobInput.textContent = userJob;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
