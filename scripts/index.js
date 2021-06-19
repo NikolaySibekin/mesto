2ecf4ec8079109046e986c157904dd18e426e4fa
Nikolay Sibekin <nik-sibekin@yandex.ru>
2021-06-19 17:47:52 +0300

    fix: first commit

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,26 @@ const popupEditButtonElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-button');
+let nameInput = document.edit.elements.name;
+let jobInput = document.edit.elements.job;
+
+const pageToInput = function() {
+  nameInput.value = document.querySelector('.profile__heading').textContent;
+  jobInput.value = document.querySelector('.profile__sub-heading').textContent;
+}
 
 const togglePopupVisibility = function() {
-  popupElement.classList.toggle('popup_is-opened');
+  popupElement.classList.toggle('popup_opened');
+  pageToInput();
+}
+
+const formSubmitHandler = function(evt) {
+    evt.preventDefault();
+    document.querySelector('.profile__heading').value = nameInput.textContent;
+    document.querySelector('.profile__sub-heading').value = jobInput.textContent;
+    togglePopupVisibility ;
 }
 
 popupEditButtonElement.addEventListener('click', togglePopupVisibility);
 popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
-popupSubmitButtonElement.addEventListener('click', togglePopupVisibility);
-
-let formElement = document.querySelector('form');
-let nameInput = formElement.elements.name;
-let jobInput = formElement.elements.job;
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-
-    let userName = nameInput.value;
-    let userJob = jobInput.value;
-
-    nameInput = formElement.elements.name;
-    jobInput = formElement.elements.job;
-    
-    nameInput.textContent = userName;
-    jobInput.textContent = userJob;
-}
-
-formElement.addEventListener('submit', formSubmitHandler);
+popupSubmitButtonElement.addEventListener('submit', formSubmitHandler);
